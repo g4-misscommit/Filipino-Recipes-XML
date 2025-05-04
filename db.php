0ef0d6bedc0c25de6b274be47c898ee23f1b64cd
NeTTieX03 <manzanojanet322@gmail.com>
2025-05-04 14:31:42 +0800

    tatlo

--- db.php
+++ db.php
@@ -2,11 +2,11 @@
 // Database connection settings
 $host = 'localhost'; // or your database server address
 $user = 'root';      // your database username
-$password = 'Shimpaishinaide#999';      // your database password
-$dbname = 'FilipinoRecipes'; // your database name
+$password = 'Janet.2001';      // your database password
+$dbname = 'filipinorecipes'; // your database name
 
 // Create connection
-$conn = new mysqli($host, $user, $password, $dbname);
+$conn = new mysqli($host, $user, $password, $dbname, 3307);
 
 // Check connection
 if ($conn->connect_error) {
